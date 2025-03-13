44d68a974f50ba5be1682d06e89d14af4a886e05
Mesole Emmanuel <meshmmanuel@gmail.com>
2025-03-13 10:30:50 +0100

    new changes

--- constants/paymentProviders.ts
+++ constants/paymentProviders.ts
@@ -13,29 +13,89 @@ interface PaymentProvider {
 
 export const paymentProviders: PaymentProvider[] = [
     {
-        id: "paystack",
-        name: "Paystack",
+        id: 'paystack',
+        name: 'Paystack',
         countries: [
-            { code: "NG", name: "Nigeria", currency: "NGN", currencyName: "Nigerian Naira" },
-        ]
+            {
+                code: 'NG',
+                name: 'Nigeria',
+                currency: 'NGN',
+                currencyName: 'Nigerian Naira',
+            },
+        ],
     },
     {
-        id: "flutterwave",
-        name: "Flutterwave",
+        id: 'flutterwave',
+        name: 'Flutterwave',
         countries: [
-            { code: "BF", name: "Burkina Faso", currency: "XOF", currencyName: "West African CFA Franc" },
-            { code: "CM", name: "Cameroon", currency: "XAF", currencyName: "Central African CFA Franc" },
-            { code: "CI", name: "Cote d'Ivoire", currency: "XOF", currencyName: "West African CFA Franc" },
-            { code: "GH", name: "Ghana", currency: "GHS", currencyName: "Ghanaian Cedi" },
-            { code: "KE", name: "Kenya", currency: "KES", currencyName: "Kenyan Shilling" },
-            { code: "NG", name: "Nigeria", currency: "NGN", currencyName: "Nigerian Naira" },
-            { code: "RW", name: "Rwanda", currency: "RWF", currencyName: "Rwandan Franc" },
-            { code: "SN", name: "Senegal", currency: "XOF", currencyName: "West African CFA Franc" },
-            { code: "TZ", name: "Tanzania", currency: "TZS", currencyName: "Tanzanian Shilling" },
-            { code: "UG", name: "Uganda", currency: "UGX", currencyName: "Ugandan Shilling" },
-            { code: "ZM", name: "Zambia", currency: "ZMW", currencyName: "Zambian Kwacha" }
-        ]
-    }
+            {
+                code: 'BF',
+                name: 'Burkina Faso',
+                currency: 'XOF',
+                currencyName: 'West African CFA Franc',
+            },
+            {
+                code: 'CM',
+                name: 'Cameroon',
+                currency: 'XAF',
+                currencyName: 'Central African CFA Franc',
+            },
+            {
+                code: 'CI',
+                name: "Cote d'Ivoire",
+                currency: 'XOF',
+                currencyName: 'West African CFA Franc',
+            },
+            {
+                code: 'GH',
+                name: 'Ghana',
+                currency: 'GHS',
+                currencyName: 'Ghanaian Cedi',
+            },
+            {
+                code: 'KE',
+                name: 'Kenya',
+                currency: 'KES',
+                currencyName: 'Kenyan Shilling',
+            },
+            {
+                code: 'NG',
+                name: 'Nigeria',
+                currency: 'NGN',
+                currencyName: 'Nigerian Naira',
+            },
+            {
+                code: 'RW',
+                name: 'Rwanda',
+                currency: 'RWF',
+                currencyName: 'Rwandan Franc',
+            },
+            {
+                code: 'SN',
+                name: 'Senegal',
+                currency: 'XOF',
+                currencyName: 'West African CFA Franc',
+            },
+            {
+                code: 'TZ',
+                name: 'Tanzania',
+                currency: 'TZS',
+                currencyName: 'Tanzanian Shilling',
+            },
+            {
+                code: 'UG',
+                name: 'Uganda',
+                currency: 'UGX',
+                currencyName: 'Ugandan Shilling',
+            },
+            {
+                code: 'ZM',
+                name: 'Zambia',
+                currency: 'ZMW',
+                currencyName: 'Zambian Kwacha',
+            },
+        ],
+    },
 ];
 
 export type { Country, PaymentProvider };
